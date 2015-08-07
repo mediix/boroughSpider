5657f0ac52aa65d1a279693d3c32035334f0b0b9
medi <mnazari@gvhomes.com>
2015-08-07 11:22:57 -0700

    Added 10 API keys for reverse geocoder

--- scripts/borough_address_reverse_geocode.py
+++ scripts/borough_address_reverse_geocode.py
@@ -2,7 +2,18 @@ import MySQLdb, sys
 from geopy.geocoders import GoogleV3
 from time import sleep
 
-def address_geocoder():
+API_keys = ['AIzaSyDNQZxPSQ_gfpQE9sVGmIwKOnbRjcEilLA',
+            'AIzaSyAoSpAegjLFNPipFLHD8MtmpH54YomhReg',
+            'AIzaSyDVFoT_bWDgBArqiYGsf33GYfBfxZn4qbM',
+            'AIzaSyDVq6OFHPVoySHcVTKcFO6t_7MVcwlsX6k',
+            'AIzaSyDOpsJAlBbyhY2sZBPXom8HuHmDsegfIwU',
+            'AIzaSyBwYIzekGVDThlIQg4amlqgDp-YI_NFjb0',
+            'AIzaSyCJs1jceSdVwvVU84JxR785bxJhEnuLONU',
+            'AIzaSyD2YF6Fq5_VFZWaahsAFU_eBfe7DTFVsZU',
+            'AIzaSyCWHVugW0e6jYgIBAAbRtHIb6sEgz2WDFk',
+            'AIzaSyCaRBOchmd_Ge726PnAot9NwlO_u6kmnDA']
+
+def address_geocoder(API_key=None):
   """
   database: target database
   table: target table
@@ -28,15 +39,11 @@ def address_geocoder():
   addrs = { t[0]: t[1:] for t in addrs }
   addrs = { key: (value[0].encode('utf-8') if value else None) for key, value in addrs.items() }
 
-  g_0 = GoogleV3('AIzaSyCaENUu85uuSC6h8-1DhJ5H29R0O0WrFqA')
-  g_1 = GoogleV3('AIzaSyAoSpAegjLFNPipFLHD8MtmpH54YomhReg')
-  g_2 = GoogleV3('AIzaSyBJxFUFQcmYmugsxEoytQJzvdlhhvI0aVo')
-  g_3 = GoogleV3('AIzaSyDVq6OFHPVoySHcVTKcFO6t_7MVcwlsX6k')
-  g_4 = GoogleV3('AIzaSyChq8S0qHCjpBhZD_qPoTESN1dnrSQ_zZk')
+  geo = GoogleV3(API_key)
 
   for key, value in addrs.items():
     try:
-      response = g_1.geocode(value, exactly_one=True)
+      response = geo.geocode(value, exactly_one=True)
       if response is not None:
         addr, (lat, lon) = response
         # print "Address: {0}, Latitude: {1}, Longitutde: {2}".format(addr, lat, lon)
@@ -56,4 +63,5 @@ def address_geocoder():
   conn.close()
 
 if __name__ == '__main__':
-  address_geocoder()
+  for key in API_keys:
+    address_geocoder(key)
