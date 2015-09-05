19f44296f81a216a5a77eb68a6a929cdb2642240
medi <mnazari@gvhomes.com>
2015-09-05 15:02:21 -0700

    extras.py added

---
+++ boroughSpider/extras.py
@@ -0,0 +1,63 @@
+import MySQLdb
+
+default = ''
+
+db_Error = MySQLdb.Error
+# con = MySQLdb.connect(user='scraper',
+#                  passwd='12345678',
+#                  db='research_uk',
+#                  host='granweb01',
+#                  charset='utf8',
+#                  use_unicode=True)
+
+con = MySQLdb.connect(user='mehdi',
+                  passwd='pashmak.mN2',
+                  db='research_uk_public_data',
+                  host='granweb01',
+                  charset='utf8',
+                  use_unicode=True)
+
+def check_address(address):
+  """"""
+  sql = "SELECT id FROM addresses WHERE address = 'ADDRESS';".replace('ADDRESS', address)
+  try:
+    cur.execute(sql)
+    db_response = cur.fetchone()
+    if db_response is None:
+      cur.execute("INSERT INTO addresses (address) VALUES (%s);", [address])
+      con.commit()
+      cur.execute("SELECT LAST_INSERT_ID();")
+      db_response = cur.fetchone()
+    _id = db_response[0]
+  except db_Error as err:
+    print "Error: %d: %s" % (err.args[0], err.args[1])
+
+  return _id
+
+# def store_keys(item, name):
+#     """"""
+#     file_name = file_path.replace('NAME', name)
+#     item_keys = item
+
+#     if not os.path.isfile(file_name):
+#         with open(file_name, 'w') as f:
+#             json.dump(item_keys, f)
+#     try:
+#         with open(file_name, 'r') as f:
+#             stored_keys = json.load(f)
+#     except Exception as err:
+#         print "Error opening file from store_kyes: ", err
+#         pass
+#     if stored_keys:
+#         diff = list(set(item_keys) - set(stored_keys))
+#         if diff:
+#             print "\n!!!DIFF IS: \n", diff
+#             stored_keys = stored_keys + diff
+#             with open(file_name, 'w') as f:
+#                 json.dump(stored_keys, f)
+#     else:
+#         stored_keys = item_keys
+#         with open(file_name, 'w') as f:
+#             json.dump(stored_keys, f)
+
+
--- scripts/scrapydAPI.py
+++ scripts/scrapydAPI.py
@@ -1,10 +1,7 @@
-import os
-# from subprocess import Popen, PIPE
+from requests import post
+from dateutil import parser
 from datetime import date, datetime, timedelta
 from scrapyd_api import ScrapydAPI
-
-p = 'boroughSpider'
-scrapyd = ScrapydAPI('http://localhost:6800')
 
 def create_dates(start, end, delta):
   curr = start
@@ -12,20 +9,25 @@ def create_dates(start, end, delta):
     yield curr
     curr += delta
 
-if __name__ == '__main__':
-  months = []
-  for result in create_dates(date(2013, 1, 1), date.today(), timedelta(days = 31)):
-    months.append(result.strftime('%b %y'))
+months = []
+for result in create_dates(date(2013, 8, 1), date(date.today().year, (date.today().month+1)%12, 1), timedelta(days = 31)):
+  months.append(result.strftime('%b %y'))
 
-  ## Delete Version
-  # [scrapyd.delete_version(p, ver) for ver in scrapyd.list_versions(p)]
+#---------------------------------------------------------------------------------
+p = 'boroughSpider'
+url = 'http://localhost:6800/schedule.json'
+scrapyd = ScrapydAPI('http://localhost:6800')
 
-  ## Spiders list
-  spiders = [spider.encode('utf-8') for spider in scrapyd.list_spiders(p)]
+# [scrapyd.delete_version(p, ver) for ver in scrapyd.list_versions(p)]
 
-  for month in months[:2]:
-    for spider in spiders:
-      if spider == 'londSpider':
-        scrapyd.schedule(p, spider)
+for elem in scrapyd.list_jobs(p)['running']:
+  print elem
+  scrapyd.cancel(p, elem.get('id'))
 
+# spiders = [spider.encode('utf-8') for spider in scrapyd.list_spiders(p)]
 
+# for month in months[::-1]:
+#   for sp in spiders:
+#     if sp in 'soutSpider':
+#       data = {'project':p, 'spider':sp, 'month':month}
+#       post(url, data=data)
